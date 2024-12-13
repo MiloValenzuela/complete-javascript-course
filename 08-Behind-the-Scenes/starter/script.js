b816d657aa32d70218ddc30f324ebdeb4f7e28fe
MiloValenzuela <valenzuelagarza@gmail.com>
2024-12-12 22:45:36 -0500

    Shallow vs deep copies

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,45 @@
 'use strict';
+
+const jessica1 = {
+    firstName: 'Jessica',
+    lastName: 'Williams',
+    age: 27,
+};
+
+function marryPerson(person, newLastName) {
+    person.lastName = newLastName;
+    return person;
+}
+
+const marriedJessica = marryPerson(jessica1, 'Davis');
+
+// const marriedJessica = jessica;
+// marriedJessica.lastName = 'Davis';
+
+console.log('Before:', jessica1);
+console.log('After:', marriedJessica);
+
+const jessica = {
+    firstName: 'Jessica',
+    lastName: 'Williams',
+    age: 27,
+    family: ['Alice', 'Bob'],
+};
+
+// Shallow Copy
+const jessicaCopy = {...jessica};
+jessicaCopy.lastName = 'Davis';
+
+// jessicaCopy.family.push('Mary');
+// jessicaCopy.family.push('John');
+
+// console.log('Before:',jessica);
+// console.log('After:', jessicaCopy);
+
+// Deep Clone or copy
+const jessicaClone = structuredClone(jessica);
+jessicaCopy.family.push('Mary');
+jessicaCopy.family.push('John');
+
+console.log('Original:',jessica);
+console.log('Clone:', jessicaClone);
