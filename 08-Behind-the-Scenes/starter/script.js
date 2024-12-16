8419f7e589ea4cd0a782d1439b53632df8788750
MiloValenzuela <valenzuelagarza@gmail.com>
2024-12-16 12:35:28 -0500

    Scooping practice

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,5 +1,5 @@
 'use strict';
-
+/*
 const jessica1 = {
     firstName: 'Jessica',
     lastName: 'Williams',
@@ -43,3 +43,37 @@ jessicaCopy.family.push('John');
 
 console.log('Original:',jessica);
 console.log('Clone:', jessicaClone);
+
+*/
+
+function calcAge(birthYear) {
+    const age = 2037 - birthYear;
+    
+    function printAge() {
+        let output = `${firstName}, You are ${age}, born in ${birthYear}`;
+        console.log(output);
+
+        if (birthYear >= 1981 && birthYear <= 1996) {
+            var millenial = true;
+            const firstName ='Steve'
+            const str = `Oh, and you're a millenial, ${firstName}`;
+            console.log(str);
+
+            function add(a, b) {
+                return a + b;
+            }
+
+            output = 'NEW OUTPUT'
+        }
+        console.log(millenial);
+        // console.log(add(2, 3));
+        console.log(output);
+    }
+    printAge();
+
+    return age;
+}
+
+const firstName = 'Jonas';
+calcAge(1987);
+// console.log(age);
