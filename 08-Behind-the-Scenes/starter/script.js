fab698360a821594114971cbc8faa37af8ae0ec3
MiloValenzuela <valenzuelagarza@gmail.com>
2024-12-18 20:18:34 -0600

    regular vs. Arrow Functions

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -107,8 +107,6 @@ let y = 2;
 const z = 3;
 
 
-*/
-
 
 
 const calcAge = function(birthYear) {
@@ -123,15 +121,6 @@ const calcAgeArrow = birthYear => {
 };
 calcAgeArrow(1980);
 
-const jonas ={
-    year: 1991,
-    calcAge: function () {
-        console.log(this);
-        console.log(2037 - this.year);
-    }
-}
-jonas.calcAge();
-
 const matilda = {
     year: 2017,
 };
@@ -141,3 +130,43 @@ matilda.calcAge();
 
 const f = jonas.calcAge;
 f(1987);
+
+*/
+
+
+
+const jonas = {
+    firsName: 'Jonas',
+    year: 1991,
+    calcAge: function () {
+        // console.log(this);
+        console.log(2037 - this.year);
+
+        const self = this;
+        const isMillenial = function() {
+            console.log(self);
+            console.log(self.year >= 1981 && self.year <= 1996);
+            // console.log(this.year >= 1981 && this.year <= 1996);
+        };
+        isMillenial();
+    },
+    greet: function() { console.log(`Hey ${this.firsName}`);
+    },
+};
+jonas.greet();
+jonas.calcAge();
+
+// Arguments keyword
+const addExpr = function(a,b) {
+    console.log(arguments);
+    return a + b;
+};
+addExpr(2, 5);
+addExpr(2, 4, 8, 12);
+
+var addArrow = (a, b) => {
+    console.log(arguments);
+    return a + b;
+}; 
+
+addArrow(1,5,8);
