86900b8c0e3ffe8e930cef7bee239e6f52a167c1
MiloValenzuela <valenzuelagarza@gmail.com>
2025-02-05 13:17:03 -0600

    Logical Assingment Operators

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -162,3 +162,26 @@ console.log(guests);
 
 const guestsCorrect = restaurant.numGuests ?? 10
 console.log(guestsCorrect);
+
+const rest1 = {
+  name: 'Capri',
+  numGuests: 20,
+};
+
+const rest2 = {
+  name: 'La Piazza',
+  owner: 'Giovanni Rossi',
+};
+
+// rest2.numGuests = rest1.numGuests || 10;
+// rest2.numGuests = rest2.numGuests || 10;
+rest1.numGuests ??=10
+rest2.numGuests ??=10
+
+// rest2.owner = rest1.owner && '<ANONYMOUS>'
+// rest2.owner = rest2.owner && '<ANONYMOUS>'
+rest1.owner &&= '<ANONYMOUS>'
+rest2.owner &&= '<ANONYMOUS>'
+
+console.log(rest1);
+console.log(rest2);
