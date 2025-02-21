b06da8478f9ccfdec9453d704cf3781ede2e068d
MiloValenzuela <valenzuelagarza@gmail.com>
2025-02-21 11:21:19 -0600

    looping Object keys, values and entries

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -141,3 +141,26 @@ for (const item of menu) console.log(item);
 for (const [i, el] of menu.entries()) {
   console.log(`${i + 1}: ${el}`);
 }
+
+// preoperty names
+const properties = Object.keys(openingHours)
+console.log(properties);
+
+let openStr = `We are open on ${properties.length} days: `;
+
+for (const day of properties) {
+  openStr += `${day}, `;
+}
+console.log(openStr);
+
+// Property VALUES
+const value = Object.values(openingHours);
+console.log(value);
+
+// Entire object
+const entries = Object.entries(openingHours);
+// console.log(entries);
+
+for (const [key, {open, close}] of entries) {
+  console.log(`On ${key} we open at ${open} and close at ${close}`);
+}
