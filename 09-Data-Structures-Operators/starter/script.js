2115d8bcc624af42f528a6c7e96e10135368de48
MiloValenzuela <valenzuelagarza@gmail.com>
2025-02-26 18:51:41 -0600

    Maps: fundametals

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -244,4 +244,26 @@ console.log('Difference mexican',uniqueMexicanFoods);
 const uniqueItalianAndMexicanFoods = italianFoods.symmetricDifference(mexicanFoods)
 console.log(uniqueItalianAndMexicanFoods);
 
-console.log(italianFoods.isDisjointForm(mexicanFoods));
+
+const rest = new Map();
+rest.set('name', 'Classico Italiano');
+rest.set(1, 'Firenze, Italy')
+console.log(rest.set(2, 'Lisbon, Portugal'));
+
+rest
+.set('categories', ['Italian', 'Oizzeria', 'Vegetarian', 'Origanic'])
+.set('open', 11)
+.set('close', 23)
+.set(true, 'We are open :D')
+.set(false, 'We are closed :(');
+
+console.log(rest.get('name'));
+console.log(rest.get(true));
+console.log(rest.get(1));
+
+const time = 8; 
+console.log(rest.get(time > rest.get('open') && time < rest.get('close')));
+
+console.log(rest.has('categories'));
+rest.delete(2);
+console.log(rest.get([1, 2]));
