494593db6f24008262c73070b0d734973393cde7
MiloValenzuela <valenzuelagarza@gmail.com>
2025-03-14 12:46:32 -0600

    bind method plus challenge

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -177,3 +177,22 @@ lufthansa.buyPlane = function() {
 // lufthansa.buyPlane();
 
 document.querySelector('.buy').addEventListener('click', lufthansa.buyPlane.bind(lufthansa));
+
+// Partial applications
+const addTax = (rate, value) => value + value * rate;
+console.log(addTax(0.1, 200));
+
+const addVAT = addTax.bind(null, 0.23);
+// (rate, value) => value + value * 0.23;
+
+console.log(addVAT(100));
+console.log(addVAT(23));
+
+const addTaxRate = function(rate) {
+    return function(value) {
+        return value + value * rate;
+    } 
+}
+const addVAT2 = addTaxRate(0.23);
+console.log(addVAT2(100));
+console.log(addVAT2(23));
