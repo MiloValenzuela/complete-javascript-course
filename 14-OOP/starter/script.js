52498d8e29a050d239c67d33405ef4e44af57429
MiloValenzuela <valenzuelagarza@gmail.com>
2025-06-02 17:41:49 -0600

    Prototypes

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -5,10 +5,10 @@ const Person = function(firstName, birthYear) {
     this.firstName = firstName;
     this.birthYear = birthYear;
 
-    this.calcAge = function() {
-        console.log(2037 - this.birthYear);
-    }
-
+    // Never do this!!!
+    // this.calcAge = function() {
+    //     console.log(2037 - this.birthYear);
+    // }
 };
 
 const jonas = new Person('Jonas', 1991);
@@ -24,3 +24,21 @@ const jack = new Person('Jack', 1975);
 console.log(matilda, jack);
 
 console.log(jonas instanceof Person);
+
+// Prototypes
+Person.prototype.calcAge = function() {
+    console.log(2037 - this.birthYear);
+}
+
+jonas.calcAge();
+matilda.calcAge();
+
+console.log(jonas.__proto__);
+console.log(jonas.__proto__ === Person.prototype);
+
+console.log(Person.prototype.isPrototypeOf(jonas));
+console.log(Person.prototype.isPrototypeOf(matilda));
+console.log(Person.prototype.isPrototypeOf(Person));
+
+Person.prototype.species = 'Homo Sapiens';
+console.log(jonas.species, matilda.species);
