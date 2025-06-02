e7cefc23bc8233a99319de787aa7c6656b477124
MiloValenzuela <valenzuelagarza@gmail.com>
2025-06-02 17:23:24 -0600

    construction of functions with OOP

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -1,9 +1,15 @@
 'use strict';
 
 const Person = function(firstName, birthYear) {
+    // Instance proporeties
     this.firstName = firstName;
     this.birthYear = birthYear;
-}
+
+    this.calcAge = function() {
+        console.log(2037 - this.birthYear);
+    }
+
+};
 
 const jonas = new Person('Jonas', 1991);
 console.log(jonas);
@@ -16,3 +22,5 @@ console.log(jonas);
 const matilda = new Person('Matilda', 2017);
 const jack = new Person('Jack', 1975);
 console.log(matilda, jack);
+
+console.log(jonas instanceof Person);
