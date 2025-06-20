114c111196ec6a310727f24a827beb40da3fc39d
MiloValenzuela <valenzuelagarza@gmail.com>
2025-06-19 21:30:11 -0600

    Prototypal Inheritance on Built-In ojects

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -48,3 +48,20 @@ console.log(jonas.hasOwnProperty("species"));
 
 console.log(jonas.__proto__);
 console.log(jonas.__proto__.__proto__);
+console.log(jonas.__proto__.__proto__.__proto__);
+
+console.log(Person.prototype.constructor);
+
+const arr = [3, 6, 4, 5, 9, 3];
+console.log(arr.__proto__ === Array.prototype);
+
+console.log(arr.__proto__.__proto__);
+
+Array.prototype.unique = function() {
+    return [...new Set(this)]
+};
+
+console.log(arr.unique());
+
+const h1 = document.querySelector('h1');
+console.dir(x => x + 1);
