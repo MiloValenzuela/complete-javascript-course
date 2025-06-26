25374491d95254ee5fbb19bc425208f1303732da
MiloValenzuela <valenzuelagarza@gmail.com>
2025-06-25 23:33:13 -0600

    intro to Setters and Getters

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -139,3 +139,21 @@ jessica.greet();
 // 1. Classes are NOT hoisted
 // 2. Classes are first-class citizes
 // 3. Classes are executed in strict mode
+
+const account = {
+    owner: 'Jonas',
+    movements: [200, 530, 120, 300],
+
+    get latest() {
+        return this.movements.slice(-1).pop();
+    },
+
+    set latest(mov) {
+        this.movements.push(mov);
+    }
+};
+
+console.log(account.latest);
+
+account.latest = 50;
+console.log(account.movements);
