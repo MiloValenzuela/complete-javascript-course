ceec1eab380480fd9d1c63c6f7284aa8363c98f1
MiloValenzuela <valenzuelagarza@gmail.com>
2025-06-26 21:45:24 -0600

    Setters and Getters

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -123,9 +123,23 @@ class PersonCL {
     greet() {
         console.log(`Hey ${this.firstName}`);
     }
+
+    get age() {
+        return 2037 - this.birthYear
+    }
+
+    set fullName(name) {
+        console.log(name);
+        if (name.includes(' ')) this._fullName = name;
+        else alert(`${name} is not a full name!`)
+    }
+
+    get fullName() {
+        return this._fullName;
+    }
 }
 
-const jessica =new PersonCL('Jessica', 1996);
+const jessica =new PersonCL('Jessica Davis', 1996);
 console.log(jessica);
 jessica.calcAge();
 
@@ -139,6 +153,8 @@ jessica.greet();
 // 1. Classes are NOT hoisted
 // 2. Classes are first-class citizes
 // 3. Classes are executed in strict mode
+
+const walter = new PersonCL('Walter White',1965)
 
 const account = {
     owner: 'Jonas',
